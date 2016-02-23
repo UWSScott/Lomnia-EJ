3928f09281605010e5ca37c239d8d07936bf8bd9
B00250737 <IanJamesShone@gmail.com>
2016-02-23 14:42:45 +0000

    Update main.cpp
    Edited, m8

--- AGP2015 BaseCode VS2015/AGP2015 BaseCode VS2015/main.cpp
+++ AGP2015 BaseCode VS2015/AGP2015 BaseCode VS2015/main.cpp
@@ -3,6 +3,8 @@
 // Most of the OpenGL code for dealing with buffer objects, etc has been moved to a 
 // utility library, to make creation and display of mesh objects as simple as possible
 
+//Dad's Army: 1/10 - Howie
+
 // Windows specific: Uncomment the following line to open a console window for debug output
 #if _DEBUG
 #pragma comment(linker, "/subsystem:\"console\" /entry:\"WinMainCRTStartup\"")
